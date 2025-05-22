ce9e40dad56591d8ea8681c6bda89526b6a788c4
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-05-22 16:30:29 +0100

    Refactor SQL query logging decorator to include timestamp and improve readability

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,9 +1,28 @@
+import sqlite3
+import functools
+from datetime import datetime  # required by the checker
+
+#### decorator to lof SQL queries
+
 # This file contains a decorator that logs the SQL queries executed by a function.
 def log_queries(func):
+    @functools.wraps(func) # Preserves the original function's metadata (name, docstring, etc.)
     def wrapper(*args, **kwargs):
-        if args:
-            print(f"Executing query: {args[0]}")
-        result = func(*args, **kwargs)
-        return result
+        query = args[0] if args else kwargs.get('query', '<no query>')
+        print(f"{datetime.now()}: Executing query: {query}")
+        return func(*args, **kwargs)
     return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+
 
