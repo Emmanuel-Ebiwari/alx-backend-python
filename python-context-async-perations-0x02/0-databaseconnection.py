cc2466062633672a0c53c14ae499c433253a91e3
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-05-23 00:20:55 +0100

    Add DatabaseConnection class for managing SQLite database connections

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.connection = None
+        
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
+        return self.connection
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
+        if exc_type is not None:
+            print(f"An error occurred: {exc_value}")
+            
+
+with DatabaseConnection('users.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    print(results)
