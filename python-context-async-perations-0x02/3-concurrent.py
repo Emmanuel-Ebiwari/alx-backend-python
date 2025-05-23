73491bcfaeb5ce3c8299aede43015b6a3a0db45f
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-05-23 10:37:41 +0100

    Refactor async user fetching functions to use context management for database connections

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,30 +1,24 @@
 import aiosqlite
 import asyncio
-import functools
 
-# DB_NAME = 'users.db'
-async def async_fetch_users(conn):
-    cursor = await conn.execute('SELECT * FROM users')
-    rows = await cursor.fetchall()
-    await cursor.close()
-    return rows
-   
-async def async_fetch_older_users(conn):
-    cursor = await conn.execute('SELECT * FROM users WHERE age > ?', (40,))
-    rows = await cursor.fetchall()
-    await cursor.close()
-    return rows
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as conn:
+        cursor = await conn.execute('SELECT * FROM users')
+        rows = await cursor.fetchall()
+        await cursor.close()
+        return rows
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect('users.db') as conn:
+        cursor = await conn.execute('SELECT * FROM users WHERE age > ?', (40,))
+        rows = await cursor.fetchall()
+        await cursor.close()
+        return rows
 
 async def fetch_concurrently():
-    async with aiosqlite.connect('users.db') as conn:
-        await asyncio.gather(
-            async_fetch_users(conn),
-            async_fetch_older_users(conn)
-        )
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
 
 asyncio.run(fetch_concurrently())
-
-
-    
-
-    
