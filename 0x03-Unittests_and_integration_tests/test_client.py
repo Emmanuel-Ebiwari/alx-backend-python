a4ac686de9f7633e4a6d9beec617933dd5bdc458
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-05-29 13:12:18 +0100

    Fix test cases in TestGithubOrgClient to use correct repos_url and add test for _public_repos_url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for access_nested_map function in utils module.
 """
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 from typing import (
@@ -16,8 +16,8 @@ class TestGithubOrgClient(unittest.TestCase):
     """Test suite for the GithubOrgClient class."""
 
     @parameterized.expand([
-        ("google", {"org_url": "https://api.github.com/orgs/google"}),
-        ("abc", {"org_url": "https://api.github.com/orgs/abc"}),
+        ("google", {"repos_url": "https://api.github.com/orgs/google"}),
+        ("abc", {"repos_url": "https://api.github.com/orgs/abc"}),
     ])
     @patch('client.get_json')
     def test_org(self, org, mocked_payload, mock_get_json) -> None:
@@ -34,6 +34,17 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.return_value = mocked_payload
         self.assertEqual(result, mocked_payload)
+
+    def test_public_repos_url(self) -> None:
+        """
+        Test that the _public_repos_url property returns the correct URL.
+        """
+        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
+            expected_url = "https://api.github.com/orgs/google"
+            mock_org.return_value = {
+                "repos_url": expected_url}
+            client = GithubOrgClient("google")
+            self.assertEqual(client._public_repos_url, expected_url)
 
 
 if __name__ == "__main__":
