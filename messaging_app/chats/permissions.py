0e201f7202a55bdde64be3b220b21128da34efe4
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-06-06 14:09:04 +0100

    Implement JWT authentication with custom token views and permissions

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsSender(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.sender == request.user
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,12 +3,13 @@ from rest_framework import permissions, viewsets, status, filters
 from rest_framework.response import Response
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
+from .permissions import IsSender
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsSender]
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['sent_at']
 
@@ -16,12 +17,18 @@ class ConversationViewSet(viewsets.ModelViewSet):
         response = super().create(request, *args, **kwargs)
         return Response(response.data, status=status.HTTP_201_CREATED)
 
+    def get_queryset(self):
+        return Message.objects.filter(sender=self.request.user)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsSender]
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         return Response(response.data, status=status.HTTP_201_CREATED)
+
+    def get_queryset(self):
+        return Message.objects.filter(sender=self.request.user)
