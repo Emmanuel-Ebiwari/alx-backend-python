2511b1f9c5d5e1adc5e3534545f0ddd6b90bebb9
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-06-06 14:16:27 +0100

    Refactor permission class to use consistent import style for BasePermission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsSender(BasePermission):
+class IsSender(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.sender == request.user
